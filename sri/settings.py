35758666f7203c208fb33ab7829b6e9c01cad289
bouzal <49028204+Bouzaloussama@users.noreply.github.com>
2021-03-29 00:41:01 +0200

    Update settings.py

--- sri/settings.py
+++ sri/settings.py
@@ -17,11 +17,11 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # solvr keroku
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),)
 
 
 # Quick-start development settings - unsuitable for production
