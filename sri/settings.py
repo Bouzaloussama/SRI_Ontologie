56bebdde9f4fb9a79240e9dc6002c8d7a0c7b587
bouzal <49028204+Bouzaloussama@users.noreply.github.com>
2021-03-29 00:54:02 +0200

    Update settings.py

--- sri/settings.py
+++ sri/settings.py
@@ -14,15 +14,17 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
+#=====================================
 
 # solvr keroku
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_URL = '/static/'
 #STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),)
+   # os.path.join(BASE_DIR, 'static'),)
 
+#=====================================
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
