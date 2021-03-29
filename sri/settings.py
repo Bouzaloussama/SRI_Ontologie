2ccb1bfaa7bd733b8a9f40421816aaf31fd5a8d4
bouzal <49028204+Bouzaloussama@users.noreply.github.com>
2021-03-29 03:33:13 +0200

    Update settings.py

--- sri/settings.py
+++ sri/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # solvr keroku
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, )#'staticfiles')
 #STATIC_URL = '/static/'
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
